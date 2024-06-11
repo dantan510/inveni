9eeb4c274d46ebac872c3a8d683ace405ff92fc2
Daniel Tan <dantan510@gmail.com>
2024-06-11 16:35:12 -0700

    Configure server.ts to use express.json to parse incoming requests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,23 @@
-import express, { Express, Request, Response } from 'express'
-import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
+const app: Express = express();
+dotenv.config();
 
-const app: Express = express()
-dotenv.config()
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!!')
-})
+  res.send('Hello World!!');
+});
 
 app.get('/hi', (req: Request, res: Response) => {
-  res.send('Hi')
-})
+  res.send('Hi');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
-})
+  console.log(`Server running on ${PORT}`);
+});
