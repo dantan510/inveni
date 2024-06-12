b4b526345bcd11846f117258db42d7666b47af1b
Daniel Tan <dantan510@gmail.com>
2024-06-11 20:37:34 -0700

    Redo changes that were removed from reverting commits

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import pool from './db/db'
 import dotenv from 'dotenv';
 import cors from 'cors';
 
@@ -10,12 +11,16 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!!');
-});
-
-app.get('/hi', (req: Request, res: Response) => {
-  res.send('Hi');
+app.get('/', async (req: Request, res: Response) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM users');
+    client.release();
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing query: ', error);
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 });
 
 app.listen(PORT, () => {
