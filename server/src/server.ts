c5c13e4b8550bed27b54a067ae56da230eda427d
Daniel Tan <dantan510@gmail.com>
2024-06-11 19:33:37 -0700

    Set up connection to Postgres

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import pool from './db/db';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
@@ -8,15 +9,31 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!!');
+app.get('/', async (req: Request, res: Response) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM users');
+    client.release();
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing query: ', error);
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 });
 
-app.get('/hi', (req: Request, res: Response) => {
-  res.send('Hi');
-});
+async function connectToPostgres() {
+  try {
+    await pool.connect()
+    console.log('Postgres connected');
+  } catch (error) {
+    console.error('Error connecting to Postgres: ', error)
+    process.exit(1)
+  }
+}
+
+connectToPostgres()
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
