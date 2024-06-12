9bf32ded18894a3b7375bab2c37c1dc93da125ea
Daniel Tan <dantan510@gmail.com>
2024-06-11 20:28:35 -0700

    Revert "Set up connection to Postgres"
    This reverts commit c5c13e4b8550bed27b54a067ae56da230eda427d.

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,51 +1,19 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const db_1 = __importDefault(require("./db/db"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 dotenv_1.default.config();
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-const PORT = process.env.PORT;
-app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const client = yield db_1.default.connect();
-        const result = yield client.query('SELECT * FROM users');
-        client.release();
-        res.json(result.rows);
-    }
-    catch (error) {
-        console.error('Error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}));
-function connectToPostgres() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield db_1.default.connect();
-            console.log('Postgres connected');
-        }
-        catch (error) {
-            console.error('Error connecting to Postgres: ', error);
-            process.exit(1);
-        }
-    });
-}
-connectToPostgres();
+const PORT = process.env.PORT || 3001;
+app.get('/', (req, res) => {
+    res.send('Hello World!!');
+});
+app.get('/hi', (req, res) => {
+    res.send('Hi');
+});
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,4 @@
 import express, { Express, Request, Response } from 'express';
-import pool from './db/db';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
@@ -9,31 +8,15 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
-app.get('/', async (req: Request, res: Response) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM users');
-    client.release();
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Error executing query: ', error);
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!!');
 });
 
-async function connectToPostgres() {
-  try {
-    await pool.connect()
-    console.log('Postgres connected');
-  } catch (error) {
-    console.error('Error connecting to Postgres: ', error)
-    process.exit(1)
-  }
-}
-
-connectToPostgres()
+app.get('/hi', (req: Request, res: Response) => {
+  res.send('Hi');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
